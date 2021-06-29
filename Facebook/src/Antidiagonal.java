10134bef8ef1b89221603fa883a4f95a5b873119
tongtongyuanyuan <txue3@stevens.edu>
2021-06-29 16:58:09 -0400

    Add Facebook tag

--- Facebook/src/Antidiagonal.java
+++ Facebook/src/Antidiagonal.java
@@ -1,3 +1,53 @@
 //https://leetcode.com/discuss/interview-question/346342/facebook-onsite-matrix-antidiagonal-traverse
 //https://leetcode.com/problems/diagonal-traverse/
 //https://www.geeksforgeeks.org/return-an-array-of-anti-diagonals-of-given-nn-square-matrix/
+
+import java.util.ArrayList;
+import java.util.*;
+
+
+
+    //Solution1. T = O(N*M)
+public class Antidiagonal {
+        public static void main(String[] args) {
+            Antidiagonal main = new Antidiagonal();
+
+//            int[][] matrix = new int[][]{{12, 7, 21, 31, 11},
+//                    {45, -2, 14, 27, 19},
+//                    {-3, 15, 36, 71, 26},
+//                    {4, -13, 55, 34, 15}};
+
+//            System.out.println(main.diagonal(matrix));
+
+
+            int[][] matrix = new int[][]{{1, 2, 3},
+                    {4, 5, 6},
+                    {7, 8, 9}};
+
+            System.out.println(main.diagonal(matrix));
+        }
+
+        public List<List<Integer>> diagonal(int[][] matrix) {
+            List<List<Integer>> result = new ArrayList<>();
+
+            int m = matrix.length;
+            int n = matrix[0].length; //columns
+
+            for (int i=0;i<m+n-1;i++) {
+                List<Integer> diag = new ArrayList<>();
+
+                int row = i < n ? 0 : i - n + 1;
+                int col = i < n ? i : n - 1;
+
+                while (row < m && col >= 0) {
+                    diag.add(matrix[row][col]);
+                    row++;
+                    col--;
+                }
+
+                result.add(diag);
+            }
+
+            return result;
+        }
+    }
